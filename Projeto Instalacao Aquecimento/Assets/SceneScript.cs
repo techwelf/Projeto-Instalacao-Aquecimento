7bcfe5ac8a1d367e2b08f3f75f369e4f4a263d44
lucascviveiros <lucasdicavalcanti@hotmail.com>
2018-07-02 14:37:20 +0100

    fist commit

---
+++ Projeto Instalacao Aquecimento/Assets/SceneScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneScript : MonoBehaviour {
+
+
+    public Button show;
+
+	// Use this for initialization
+	void Start () {
+        Button btn = show.GetComponent<Button>();
+        btn.onClick.AddListener(ShowObject);
+	}
+
+    private void ShowObject()
+    {
+        if (SceneManager.GetSceneByName("projeto-tese").isLoaded)
+        {
+            SceneManager.LoadScene("projeto-tese-2");
+        }
+        else if (SceneManager.GetSceneByName("projeto-tese-2").isLoaded)
+
+        {
+            SceneManager.LoadScene("projeto-tese-3");
+
+        }
+
+    }
+        // Update is called once per frame
+        void Update () {
+		
+	}
+}
