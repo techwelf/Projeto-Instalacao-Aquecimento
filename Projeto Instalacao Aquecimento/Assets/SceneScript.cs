4a7b198fa13d56fa7d691974110fa7427e858005
lucascviveiros <lucasdicavalcanti@hotmail.com>
2018-07-03 13:46:54 +0100

    Menu changes

--- Projeto Instalacao Aquecimento/Assets/SceneScript.cs
+++ Projeto Instalacao Aquecimento/Assets/SceneScript.cs
@@ -6,31 +6,69 @@ using UnityEngine.UI;
 
 public class SceneScript : MonoBehaviour {
 
-
-    public Button show;
+    public Button btn_next;
+    public Button btn_back;
 
 	// Use this for initialization
-	void Start () {
-        Button btn = show.GetComponent<Button>();
-        btn.onClick.AddListener(ShowObject);
-	}
-
-    private void ShowObject()
+	void Start ()
     {
-        if (SceneManager.GetSceneByName("projeto-tese").isLoaded)
+        Button btn = btn_next.GetComponent<Button>();
+        btn.onClick.AddListener(NextScene);
+
+        Button btn2 = btn_back.GetComponent<Button>();
+        btn2.onClick.AddListener(BackScene);
+    }
+
+    private void NextScene()
+    {
+        if (SceneManager.GetSceneByName("scene-1").isLoaded)
         {
-            SceneManager.LoadScene("projeto-tese-2");
+            SceneManager.LoadScene("scene-2");
         }
-        else if (SceneManager.GetSceneByName("projeto-tese-2").isLoaded)
+        else if (SceneManager.GetSceneByName("scene-2").isLoaded)
 
         {
-            SceneManager.LoadScene("projeto-tese-3");
+            SceneManager.LoadScene("scene-3");
 
         }
+        else if (SceneManager.GetSceneByName("scene-3").isLoaded)
+        {
+            Exit();
+        }
+    }
+
+    private void BackScene()
+    {
+
+        if (SceneManager.GetSceneByName("scene-2").isLoaded)
+        {
+            SceneManager.LoadScene("scene-1");
+        }
+        else if (SceneManager.GetSceneByName("scene-3").isLoaded)
+
+        {
+            SceneManager.LoadScene("scene-2");
+
+        }
+        else if (SceneManager.GetSceneByName("scene-1").isLoaded)
+        {
+            Exit();
+        }
+    }
+
+    private void Exit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
 
     }
-        // Update is called once per frame
-        void Update () {
-		
-	}
+
 }
